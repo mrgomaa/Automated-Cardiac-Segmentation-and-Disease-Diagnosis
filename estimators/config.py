c7b9c7105057fc2ac423cabe693bff0f62c72fb4
mgomaa <mgom3a@gmail.com>
2021-02-26 13:35:35 +0300

    modify estimator files

--- estimators/config.py
+++ estimators/config.py
@@ -4,13 +4,13 @@ import os
 
 class conf(object):
 	def __init__(self,
-				data_path = '../../processed_acdc_dataset/hdf5_files',
-				output_dir = '../../trained_models/ACDC',
+				data_path = 'processed_acdc_dataset/hdf5_files',
+				output_dir = 'trained_models/ACDC',
 				run_name = 'FCRD_ACDC',
 				batch_size = 16,
 				num_class = 4,
 				num_channels = 1,
-				num_epochs = 250,
+				num_epochs = 10,
 				learning_rate = 1e-3,
 				prediction_batch_size = 16,
 				load_pretrained_model_from = None,
